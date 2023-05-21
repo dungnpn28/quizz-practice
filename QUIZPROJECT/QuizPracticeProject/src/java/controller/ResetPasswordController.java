b62520f43c6a15a8f472769814d90f3a962efb53
Vietbl <vietblhe171037@fpt.edu.vn>
2023-05-21 22:30:17 +0700

    Signin added and resetpassword added

---
+++ QUIZPROJECT/QuizPracticeProject/src/java/controller/ResetPasswordController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import dal.UserDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import model.User;
+
+/**
+ *
+ * @author Dell
+ */
+public class ResetPasswordController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession sessions = req.getSession();
+        String password = req.getParameter("password");
+        User usertoreset = (User) sessions.getAttribute("usertoreset");
+        UserDAO p = new UserDAO();
+        
+        p.updatePassword(usertoreset.getAccount(), password);
+        sessions.removeAttribute("usertoreset");
+        req.getRequestDispatcher("Home.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       req.getRequestDispatcher("Resetpassword.jsp").forward(req, resp);
+    }
+    
+}
---
+++ QUIZPROJECT/QuizPracticeProject/src/java/model/User.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Dell
+ */
+public class User {
+    private int id;
+    private String account;
+    private String password;
+    private int role_id;
+
+    public User(int id, String account, String password, int role_id) {
+        this.id = id;
+        this.account = account;
+        this.password = password;
+        this.role_id = role_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+   
+    
+}
