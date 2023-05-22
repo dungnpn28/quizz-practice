d5dd47fae391fcfbcf0435c323f42823b7d2bede
dungnpn <dungnpn28@gmail.com>
2023-05-22 12:26:29 +0700

    add various files

---
+++ QuizPracticeProject_dungnpn/src/java/controller/SearchBySubjectController.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import dal.ExamDAO;
+import dal.SubjectDAO;
+import java.util.List;
+import model.Exam;
+import model.Subject;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class SearchBySubjectController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        int subjectId = Integer.parseInt(request.getParameter("subjectId"));
+        SubjectDAO sDAO = new SubjectDAO();
+        List<Subject> subjectList = sDAO.getSubjects();
+        ExamDAO eDAO = new ExamDAO();
+        if (subjectId == 0) {
+            List<Exam> examList = eDAO.getExamByUserID(1);
+            request.setAttribute("examList", examList);
+            request.setAttribute("subjectList", subjectList);
+            request.getRequestDispatcher("SimulationExam.jsp").forward(request, response);
+        } else {
+            List<Exam> examList = eDAO.getExamByUserIDandSubID(1, subjectId);
+            request.setAttribute("examList", examList);
+            request.setAttribute("subjectList", subjectList);
+            request.getRequestDispatcher("SimulationExam.jsp").forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
