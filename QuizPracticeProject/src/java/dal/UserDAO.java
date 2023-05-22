18e4972c7f666d48a472ca5141e3786e57652be5
Vietbl <vietblhe171037@fpt.edu.vn>
2023-05-22 11:10:11 +0700

    fixx

--- QuizPracticeProject/src/java/dal/UserDAO.java
+++ QuizPracticeProject/src/java/dal/UserDAO.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dal;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
