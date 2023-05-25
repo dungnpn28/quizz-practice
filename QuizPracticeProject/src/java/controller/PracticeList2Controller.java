5ea6758ffb435ea4b459bd95b9d4e17db9aa3b4b
nmdai <dainmhe171402@fpt.edu.vn>
2023-05-25 10:56:52 +0700

    add file

---
+++ QuizPracticeProject/src/java/controller/PracticeList2Controller.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dal.ExamDAO;
+import dal.SubjectDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+import model.Exam;
+import model.Subject;
+import model.User;
+
+/**
+ *
+ * @author dai
+ */
+public class PracticeList2Controller extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            HttpSession session = request.getSession();
+            User x = (User) session.getAttribute("user");
+            int subjectId = Integer.parseInt(request.getParameter("subjectId"));
+            SubjectDAO sDAO = new SubjectDAO();
+            List<Subject> subjectList = sDAO.getSubjects();
+            ExamDAO eDAO = new ExamDAO();
+            if (subjectId == 0) {
+                List<Exam> examList = eDAO.getExamByUserID(x.getId());
+                request.setAttribute("examList", examList);
+                request.setAttribute("subjectList", subjectList);
+                request.getRequestDispatcher("PracticeList.jsp").forward(request, response);
+            } else {
+                List<Exam> examList = eDAO.getExamByUserIDandSubID(x.getId(), subjectId);
+                request.setAttribute("examList", examList);
+                request.setAttribute("subjectList", subjectList);
+                request.getRequestDispatcher("PracticeList.jsp").forward(request, response);
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ QuizPracticeProject/src/java/controller/PracticeListController.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import dal.ExamDAO;
+import dal.PracticeListDAO;
+import dal.SubjectDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+import model.Exam;
+import model.Subject;
+import model.User;
+
+/**
+ *
+ * @author dai
+ */
+public class PracticeListController extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+        PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
+        User x = (User) session.getAttribute("user");
+        PracticeListDAO eDAO = new PracticeListDAO();
+        SubjectDAO sDAO = new SubjectDAO();
+        List<Subject> subjectList = new ArrayList<>();
+        subjectList = sDAO.getSubjects();
+        List<Exam> examList = new ArrayList<>();
+        examList = eDAO.getExamByUserID(x.getId());
+        if (examList.isEmpty() || examList == null) {
+            request.getRequestDispatcher("PracticeList.jsp").include(request, response);
+        } else {
+
+            request.setAttribute("examList", examList);
+            request.setAttribute("subjectList", subjectList);
+            request.getRequestDispatcher("PracticeList.jsp").forward(request, response);
+
+        }
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
