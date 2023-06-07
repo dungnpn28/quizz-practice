200d669bd4d1058a4efcf17a3c42c5e0716dcf87
Vietbl <vietblhe171037@fpt.edu.vn>
2023-06-07 16:26:23 +0700

    userlist

--- QuizPracticeProject/src/java/controller/UserListController.java
+++ QuizPracticeProject/src/java/controller/UserListController.java
@@ -4,6 +4,7 @@
  */
 package controller;
 
+import dal.RoleDAO;
 import dal.UserDAO;
 import dal.UserProfileDAO;
 import jakarta.servlet.ServletException;
@@ -12,6 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import model.Role;
 import model.User;
 import model.UserProfile;
 
@@ -23,18 +25,60 @@ public class UserListController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+        
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        UserDAO u = new UserDAO();
+       
+        String gender = req.getParameter("gender");
+        String role = req.getParameter("role");
+        String status = req.getParameter("status");
+        String search = req.getParameter("search");
+        if (gender == null || gender.isEmpty()) {
+            gender = "all";
+        }
+        if (role == null || role.isEmpty()) {
+            role = "all";
+        }
+        if (status == null || status.isEmpty()) {
+            status = "all";
+        }
+        if(search == null || search.isEmpty()){
+            search = "";
+        }
+        String index = req.getParameter("index");
+        if (index == null) {
+            index = "1";
+        }
         UserProfileDAO up = new UserProfileDAO();
+        RoleDAO r = new RoleDAO();
+        int count = up.getTotalUserProfileFilter(gender, role, status,search);//15
+        int endPage = count / 5;
+        if (count % 5 != 0) {
+            endPage++;
+        }
+        List<UserProfile> list_user_profile;
+        if (gender.equals("all") && role.equals("all") && status.equals("all")) {
+            list_user_profile = up.pagingUserList(Integer.parseInt(index),search);
+        } else {
+            list_user_profile = up.filterUserList(Integer.parseInt(index), gender, role, status,search);
+        }
+        List<Role> list_role = r.getListRole();
+        req.setAttribute("list_role", list_role);
+        req.setAttribute("endP", endPage);
+        req.setAttribute("tag", Integer.parseInt(index));
 //        List<User> list_user = u.getListUser();
-        List<UserProfile> list_user_profile = up.getListUserProfile();
+        req.setAttribute("gender", gender);
+        req.setAttribute("role", role);
+        req.setAttribute("status", status);
+        req.setAttribute("search", search);
 //        req.setAttribute("list_user", list_user);
         req.setAttribute("list_user_profile", list_user_profile);
+       
         req.getRequestDispatcher("UserList.jsp").forward(req, resp);
+        
     }
-    
+
 }
--- QuizPracticeProject/src/java/dal/UserProfileDAO.java
+++ QuizPracticeProject/src/java/dal/UserProfileDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import model.User;
 import model.UserProfile;
+
 /**
  *
  * @author ADMIN
@@ -23,7 +24,7 @@ public class UserProfileDAO extends MyDAO {
             ps.setInt(1, id);
             rs = ps.executeQuery();
             String xFull_name;
-            
+
             if (rs.next()) {
                 xFull_name = rs.getString("full_name");
 
@@ -36,7 +37,7 @@ public class UserProfileDAO extends MyDAO {
         }
         return (x);
     }
-    
+
     public void update(UserProfile x) {
         xSql = "UPDATE [dbo].[user_profile]\n"
                 + "   SET \n"
@@ -62,6 +63,35 @@ public class UserProfileDAO extends MyDAO {
         }
     }
 
+    public UserProfile getUserProfilefull(int userId) {
+       
+        try {
+            String strSelect = "select [user_id],avatar,full_name,gender,phone_number,dob,created,modified \n"
+                    + "                                        FROM user_profile LEFT JOIN [user] ON [user].id = [user_profile].[user_id] where id = 1";
+            ps = con.prepareStatement(strSelect);
+            ps.setInt(1, userId);
+            rs = ps.executeQuery();
+            UserDAO u = new UserDAO();
+
+            while (rs.next()) {
+                int user_id = rs.getInt(1);
+                String avatar = rs.getString(2);
+                String full_name = rs.getString(3);
+                int gender = rs.getInt(4);
+                String phone_number = rs.getString(5);
+                String dob = rs.getString(6);
+                String created = rs.getString(7);
+                String modified = rs.getString(8);
+                User user = u.getUserById(user_id);
+                UserProfile up = new UserProfile(user_id, avatar, full_name, gender, phone_number, dob, created, modified, user);
+                return up;
+            }
+        } catch (Exception e) {
+            System.out.println("getUserProfilefull:" + e.getMessage());
+        }
+        return null;
+    }
+
     public UserProfile getUserProfile(int userId) {
         UserProfile u = null;
         try {
@@ -77,16 +107,17 @@ public class UserProfileDAO extends MyDAO {
                 int gender = rs.getInt("gender");
                 String dob = rs.getString("dob");
                 String phone_number = rs.getString("phone_number");
-                u = new UserProfile(user_id,avatar, full_name, gender, dob, phone_number);
+                u = new UserProfile(user_id, avatar, full_name, gender, dob, phone_number);
             }
         } catch (Exception e) {
             System.out.println("getUP:" + e.getMessage());
         }
         return u;
     }
-    public List<UserProfile> getListUserProfile(){
-          List<UserProfile> data = new ArrayList<>();
-        try{
+
+    public List<UserProfile> getListUserProfile() {
+        List<UserProfile> data = new ArrayList<>();
+        try {
             String strSelect = "select [user_id],avatar,full_name,gender,phone_number,dob,created,modified from user_profile";
             ps = con.prepareStatement(strSelect);
             rs = ps.executeQuery();
@@ -96,18 +127,182 @@ public class UserProfileDAO extends MyDAO {
                 String avatar = rs.getString(2);
                 String full_name = rs.getString(3);
                 int gender = rs.getInt(4);
-                 String phone_number = rs.getString(5);
+                String phone_number = rs.getString(5);
                 String dob = rs.getString(6);
-               
+
                 String created = rs.getString(7);
                 String modified = rs.getString(8);
-                User user =  u.getUserById(id);
-                UserProfile up = new UserProfile(id,avatar,full_name,gender,phone_number,dob,created,modified,user);
+                User user = u.getUserById(id);
+                UserProfile up = new UserProfile(id, avatar, full_name, gender, phone_number, dob, created, modified, user);
                 data.add(up);
             }
-        }catch(Exception e){
-             System.out.println("getAllAccount: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("getAllAccount: " + e.getMessage());
         }
         return data;
+    }
+
+    public List<UserProfile> pagingUserList(int index, String search) {
+        List<UserProfile> data = new ArrayList<>();
+        try {
+            String strSelect = "select [user_id],avatar,full_name,gender,phone_number,dob,created,modified \n"
+                    + "                    FROM user_profile LEFT JOIN [user] ON [user].id = [user_profile].[user_id] where full_name like ? or account like ? or phone_number like ? \n"
+                    + "                    order by [user_id]\n"
+                    + "                    offset ? rows fetch next 5 rows only";
+            ps = con.prepareStatement(strSelect);
+            ps.setString(1, "%" + search + "%");
+            ps.setString(2, "%" + search + "%");
+            ps.setString(3, "%" + search + "%");
+            ps.setInt(4, (index - 1) * 5);
+            rs = ps.executeQuery();
+            UserDAO u = new UserDAO();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String avatar = rs.getString(2);
+                String full_name = rs.getString(3);
+                int gender = rs.getInt(4);
+                String phone_number = rs.getString(5);
+                String dob = rs.getString(6);
+
+                String created = rs.getString(7);
+                String modified = rs.getString(8);
+                User user = u.getUserById(id);
+                UserProfile up = new UserProfile(id, avatar, full_name, gender, phone_number, dob, created, modified, user);
+                data.add(up);
+            }
+        } catch (Exception e) {
+            System.out.println("getAllAccount: " + e.getMessage());
+        }
+        return data;
+    }
+
+    public List<UserProfile> filterUserList(int index, String gender, String role, String status, String search) {
+        List<UserProfile> data = new ArrayList<>();
+        try {
+//            String strSelect = "select [user_id],avatar,full_name,gender,phone_number,dob,created,modified from user_profile left join [user] on [user].id = [user_profile].[user_id]\n"
+//                    + "			    where role_id = ? and gender = ? and [status] = ?\n"
+//                    + "                     order by [user_id]\n"
+//                    + "                     offset ? rows fetch next 5 rows only";
+            String strSelect = "SELECT [user_id], avatar, full_name, gender, phone_number, dob, created, modified FROM user_profile LEFT JOIN [user] ON [user].id = [user_profile].[user_id] WHERE 1=1";
+
+            if (!role.equals("all")) {
+                strSelect += " and [role_id]= ?";
+            }
+            if (!gender.equals("all")) {
+                strSelect += " and [gender]= ?";
+            }
+            if (!status.equals("all")) {
+                strSelect += " and [status]= ?";
+
+            }
+            strSelect += " and (full_name like ? or account like ? or phone_number like ?) ";
+            strSelect += " ORDER BY [user_id] OFFSET ? ROWS FETCH NEXT 5 ROWS ONLY";
+            ps = con.prepareStatement(strSelect);
+            int i = 1;
+            if (!role.equals("all")) {
+                ps.setInt(i, Integer.parseInt(role));
+                i++;
+            }
+            if (!gender.equals("all")) {
+                ps.setInt(i, Integer.parseInt(gender));
+                i++;
+            }
+            if (!status.equals("all")) {
+                ps.setInt(i, Integer.parseInt(status));
+                i++;
+            }
+            ps.setString(i, "%" + search + "%");
+            i++;
+            ps.setString(i, "%" + search + "%");
+            i++;
+            ps.setString(i, "%" + search + "%");
+            i++;
+            ps.setInt(i, (index - 1) * 5);
+            rs = ps.executeQuery();
+            UserDAO u = new UserDAO();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String avatar = rs.getString(2);
+                String full_name = rs.getString(3);
+                int genders = rs.getInt(4);
+                String phone_number = rs.getString(5);
+                String dob = rs.getString(6);
+
+                String created = rs.getString(7);
+                String modified = rs.getString(8);
+                User user = u.getUserById(id);
+                UserProfile up = new UserProfile(id, avatar, full_name, genders, phone_number, dob, created, modified, user);
+                data.add(up);
+            }
+        } catch (Exception e) {
+            System.out.println("filterUserList: " + e.getMessage());
+        }
+        return data;
+    }
+
+    public int getTotalUserProfile(String search) {
+        try {
+            String strSelect = "select count(*) FROM user_profile LEFT JOIN [user] ON [user].id = [user_profile].[user_id] where full_name like ? or account like ? or phone_number like ? ";
+            ps = con.prepareStatement(strSelect);
+            ps.setString(1, "%" + search + "%");
+            ps.setString(2, "%" + search + "%");
+            ps.setString(3, "%" + search + "%");
+            rs = ps.executeQuery();
+            UserDAO u = new UserDAO();
+            while (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+            System.out.println("getTotalUserProfile: " + e.getMessage());
+        }
+
+        return 0;
+    }
+
+    public int getTotalUserProfileFilter(String gender, String role, String status, String search) {
+        try {
+            String strSelect = "select count(*) from user_profile left join [user] on [user].id = [user_profile].[user_id]  WHERE 1=1 ";
+            if (!role.equals("all")) {
+                strSelect += " and [role_id]= ?";
+            }
+            if (!gender.equals("all")) {
+                strSelect += " and [gender]= ?";
+            }
+            if (!status.equals("all")) {
+                strSelect += " and [status]= ?";
+
+            }
+            strSelect += " and (full_name like ? or account like ? or phone_number like ?) ";
+
+            ps = con.prepareStatement(strSelect);
+            int i = 1;
+            if (!role.equals("all")) {
+                ps.setInt(i, Integer.parseInt(role));
+                i++;
+            }
+            if (!gender.equals("all")) {
+                ps.setInt(i, Integer.parseInt(gender));
+                i++;
+            }
+            if (!status.equals("all")) {
+                ps.setInt(i, Integer.parseInt(status));
+                i++;
+            }
+            ps.setString(i, "%" + search + "%");
+            i++;
+            ps.setString(i, "%" + search + "%");
+            i++;
+            ps.setString(i, "%" + search + "%");
+
+            rs = ps.executeQuery();
+            UserDAO u = new UserDAO();
+            while (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+            System.out.println("getTotalUserProfileFilter: " + e.getMessage());
+        }
+
+        return 0;
     }
 }
